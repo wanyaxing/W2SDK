37f24db3b05c8188bc19b9250cd19d243d84c7e2
wanyaxing <wanyaxing@gmail.com>
2015-05-14 11:33:24 +0800

    no message

--- W2SDK/W2Config.php
+++ W2SDK/W2Config.php
@@ -16,11 +16,15 @@ class W2Config {
 	public static $API_KEY_IOS        = '2100023226814';
 	public static $SECRET_KEY_IOS     = 'b632046c0cewfwe985eabe00';
 
-	// public static $API_KEY            = '54d47832fd98c5dbd10001df';
-	// public static $SECRET_KEY         = 'c3a48b44a21e37210c3e3b7add5c1c7a';
+	public static $API_KEY            = '54d47832fd98c5dbd10001df';
+	public static $SECRET_KEY         = 'c3a48b44a21e37210c3e3b7add5c1c7a';
 
 	public static $Qiniu_bucket = 'test';
 	public static $Qiniu_domain = '7u2sdg.test.z0.glb.clouddn.com';
 	public static $Qiniu_accessKey = '_AFIydsfaRbmMRP8aO38y3C9';
 	public static $Qiniu_secretKey = 'Uv9yBLUeqsdfafmVgAybHBRbT07Jj';
+
+	public static $SMS_USER = '1234';
+	public static $SMS_PASSWD = '1234';
+
 }
