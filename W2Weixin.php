dca419e3e94a08aad568c29f0fe0d7479d75a46a
wanyaxing <wyx@wanyaxing.com>
2016-10-08 11:53:55 +0800

    *

--- W2Weixin.php
+++ W2Weixin.php
@@ -12,6 +12,8 @@ class W2Weixin {
     public static $SECRET          = null; //公众号后台-开发-基本配置-AppSecret
 
     /**
+     * https://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html
+     *
      * 第一步：用户同意授权，获取code
      * @param  string $redirect_uri 授权后回调地址，需事先设定授权域名： 公众号后台-开发-接口权限，更改授权回调域名为当前域名。
      * @param  string $scope        应用授权作用域，snsapi_base （不弹出授权页面，直接跳转，只能获取用户openid），snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息）
@@ -32,7 +34,6 @@ class W2Weixin {
 
     /**
      * 第二步：通过code换取网页授权access_token
-     * https://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html
      * 首先请注意，这里通过code换取的是一个特殊的网页授权access_token,
      * 与基础支持中的access_token（该access_token用于调用其他接口）不同。
      * 公众号可通过下述接口来获取网页授权access_token。
