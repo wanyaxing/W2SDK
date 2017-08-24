eac08c32e1bc098485dc8ca5e5b5ce3813831ec3
wanyaxing <wyx@wanyaxing.com>
2017-08-24 16:32:38 +0800

    + W2String::prettyJson 将json字符串排版以供优化展示

--- W2String.php
+++ W2String.php
@@ -525,4 +525,70 @@ class W2String {
         }
         return false;
     }
+
+    /**
+     * 将json字符串排版以供优化展示
+     * https://stackoverflow.com/questions/6054033/pretty-printing-json-with-php
+     * @param  string $json
+     * @return string
+     */
+    public static function prettyJson( $json )
+    {
+        if (!is_string($json))
+        {
+            $json = json_encode($json,JSON_UNESCAPED_UNICODE);
+        }
+        $result = '';
+        $level = 0;
+        $in_quotes = false;
+        $in_escape = false;
+        $ends_line_level = NULL;
+        $json_length = strlen( $json );
+
+        for( $i = 0; $i < $json_length; $i++ ) {
+            $char = $json[$i];
+            $new_line_level = NULL;
+            $post = "";
+            if( $ends_line_level !== NULL ) {
+                $new_line_level = $ends_line_level;
+                $ends_line_level = NULL;
+            }
+            if ( $in_escape ) {
+                $in_escape = false;
+            } else if( $char === '"' ) {
+                $in_quotes = !$in_quotes;
+            } else if( ! $in_quotes ) {
+                switch( $char ) {
+                    case '}': case ']':
+                        $level--;
+                        $ends_line_level = NULL;
+                        $new_line_level = $level;
+                        break;
+
+                    case '{': case '[':
+                        $level++;
+                    case ',':
+                        $ends_line_level = $level;
+                        break;
+
+                    case ':':
+                        $post = " ";
+                        break;
+
+                    case " ": case "\t": case "\n": case "\r":
+                        $char = "";
+                        $ends_line_level = $new_line_level;
+                        $new_line_level = NULL;
+                        break;
+                }
+            } else if ( $char === '\\' ) {
+                $in_escape = true;
+            }
+            if( $new_line_level !== NULL ) {
+                $result .= "\n".str_repeat( "\t", $new_line_level );
+            }
+            $result .= $char.$post;
+        }
+        return $result;
+    }
 }
